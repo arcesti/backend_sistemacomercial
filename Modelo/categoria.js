737187a492c000baa1dc7a1c601fd358f6ccfd9a
arcesti <arcesti15@gmail.com>
2024-11-08 06:35:34 -0300

    Achei o erro, era o nome do metodo que estava errado

--- Modelo/categoria.js
+++ Modelo/categoria.js
@@ -54,7 +54,7 @@ export default class Categoria{
 
     async editar(){
         const catDAO = new CategoriaDAO();
-        await catDAO.alterar(this);
+        await catDAO.editar(this);
     }
 }
 
